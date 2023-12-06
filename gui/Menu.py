1fa10e40627c8e858a2ac46e5ce25bc950f48ac9
Ilai-Lukas Nowikow <s0579096@saturn16.f4.htw-berlin.de>
2023-12-06 15:26:57 +0100

    Added func displayMenu, Berkan und Ilai

--- gui/Menu.py
+++ gui/Menu.py
@@ -1,8 +1,51 @@
+from termcolor import cprint, colored
+
 
 class Menu:
 
     def __init__(self) -> None:
+
+        """
+        konstruktor der klasse Menu
+    
+        # init der klassen attr.
+        # gamer_id - ist der name des spielers
+        # game_id - ist die nummer des spiels
+        # anzahl_farben - auswahl der farben mit denen gespielt wird
+        # anzahl_pos - auswahl der positionen mit denen gespielt wird
+        """
+
+        self.gamer_id      = ""
+        self.game_id       = 0
+        self.anzahl_farben = 0
+        self.anzahl_pos    = 0
+        self.auswahl       = ""
+
+
+    def getUserInput():
         pass
 
-    def showMenu(self) -> None:
-        pass
+
+
+    def displayMenu(self) -> None:
+
+        print(
+        """
+                 __,                     ,__
+             __/==+\      Spielmenü     /+==\__
+                "  "`  ================ '"  "
+            
+            +------------------------------------+
+            | Superhirn ein Logikspiel für deine |
+            |           high performance!        |
+            +------------------------------------+
+
+                    [1] Spiel starten
+                    [2] Spiel beenden
+
+        """
+        )
+
+m = Menu()
+m.displayMenu()
+
