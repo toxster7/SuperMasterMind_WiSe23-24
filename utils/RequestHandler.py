0ffa53f4d1250e2bbcb3346ea8202b9a8e468dbe
htw-berkan <82588056+htw-berkan@users.noreply.github.com>
2023-12-06 22:18:56 +0100

    Class Added 'RequestHandler' Ilai Lukas Nowikow Berkan Akcay Florian Bauer Anton Kadushkin

---
+++ utils/RequestHandler.py
@@ -0,0 +1,9 @@
+class RequestHandler:
+
+    def sendRequest(gameID, gamerID, postions, colors, value):
+        #Daten in json formatieren und dann senden. 
+        print("Ciao")
+
+    def getRequest(self):
+        print("Hallo")
+
